5a69d254f54d2771366cbe6b33191c1fbf3f02b4
Tom Sharp <tomtnt2014@gmail.com>
2026-04-27 18:59:38 +0100

    using a mapped list for characters

--- Lovestuffy.com/src/Characters.tsx
+++ Lovestuffy.com/src/Characters.tsx
@@ -1,16 +1,27 @@
 import CharacterCard from "./CharacterCard";
 import LoveStuffy from "./assets/Lovestufy.jpg";
 
+const CHARACTERS = [
+  { name: "Lovestufy", image: LoveStuffy, description: "I EAT" },
+  {
+    name: "KoKo",
+    image: LoveStuffy,
+    description: "I am a axolotl INCORECT PICTURE",
+  },
+];
+
 function Characters() {
   return (
     <div id="characters">
       <h1>Characters</h1>
-      <CharacterCard name="Lovestufy" image={LoveStuffy} description="I EAT" />
-      <CharacterCard
-        name="KoKo"
-        image={LoveStuffy}
-        description="I am a axolotl INCORECT PICTURE"
-      />
+      {CHARACTERS.map((character) => (
+        <CharacterCard
+          key={character.name}
+          name={character.name}
+          image={character.image}
+          description={character.description}
+        />
+      ))}
     </div>
   );
 }
