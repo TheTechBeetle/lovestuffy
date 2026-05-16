845f321363d2a532d342c78503f6176b540b0d16
TheTechBeetle <tomtnt2014@gmail.com>
2026-05-16 13:10:30 +0100

    Fix image height style in CharacterCard component

--- Lovestuffy.com/src/CharacterCard.tsx
+++ Lovestuffy.com/src/CharacterCard.tsx
@@ -8,8 +8,8 @@ function CharacterCard({ name, image, description }: Props) {
   return (
     <div className="card">
       <p>{name}</p>
-      <div style="height:150px">
-        <img src={image} alt={name} width="200" />
+      <div height=150px>
+        <img src={image} alt={name} width=200px />
       </div>
       <p>{description}</p>
     </div>
