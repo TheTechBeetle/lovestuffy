da8dada74274ffb86fa9375caae6939a297f959a
Tom Sharp <tomtnt2014@gmail.com>
2026-05-18 18:22:28 +0100

    _

--- Lovestuffy.com/src/CharacterCard.tsx
+++ Lovestuffy.com/src/CharacterCard.tsx
@@ -8,7 +8,7 @@ function CharacterCard({ name, image, description }: Props) {
   return (
     <div className="card">
       <p>{name}</p>
-      <div style="height:150px;;">
+      <div>
         <img src={image} alt={name} width="200" />
       </div>
       <p>{description}</p>
